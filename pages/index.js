3cb948c728bb4c91d8251d7c904eaf29d3376de6
endocytosis <brian_t.gallagher@tufts.edu>
2021-12-28 15:38:03 -0600

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -18,31 +18,24 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Mechanics &rarr;</h3>
+            <p>
+              Kinematics, Newton's Laws, Work, Energy, Power, Rotational
+              Dynamics...
+            </p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Electricity and Magnetism &rarr;</h3>
+            <p>Electrostatics, Circuits, Fields, Electromagnetism...</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Modern &rarr;</h3>
+            <p>Quantum Mechanics, Special Relativity, General Relativity</p>
           </a>
         </div>
       </main>
