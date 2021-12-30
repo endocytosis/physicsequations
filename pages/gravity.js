1d03f3a6bd9f9562096ad0aba693c662e7b13131
endocytosis <brian_t.gallagher@tufts.edu>
2021-12-30 14:37:02 -0600

    1 file modified

--- pages/gravity.js
+++ pages/gravity.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 function Gravity() {
   return (
     <div className={styles.container}>
-      <div>Gravity</div>;
+      <div>Gravity</div>
       <p>
         Newton's Universal Law of Graviation:
         <img src="img/newtongravity.svg" />
